c6f233f8144b539c52352ff8392503deb8285eb4
Önsel AYDIN <onselaydin@gmail.com>
2018-02-28 23:36:11 +0300

    home
    home page first test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,15 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    this.mesaj="1 ile 10 arasında bir sayı tahmin edin"
   }
-
+  randomSayi=Math.floor((Math.random() * 10) + 1)
+  mesaj="";
+  kontrolEt(sayi){ 
+      if(this.randomSayi==sayi){
+      this.mesaj="Tebrikler doğru bildiniz";
+      return;
+      }
+      this.mesaj="Yanlış tahmin! tekrar deneyin";
+  }
 }
